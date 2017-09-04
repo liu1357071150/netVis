0934260d67a45f2d1ef65b7d34e15192233a3bd1
Rainy <p_wq@outlook.com>
2017-09-04 10:38:16 +0800

    netVis

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,18 @@
-function Main() {
+function MainChart() {
     var main_div = $("#main");
     var mainChart = {
         width: main_div.width(),
         height: main_div.height(),
         tmp_nodes: [],
         nodes: [],
-        links: []
+        links: [],
+        rScale: d3.scale.linear().range([2, 8]),
+        bScale: d3.scale.linear().range([1, 4]),
+        rMax: 0,
+        bMax: 0
     };
 
-    d3.csv("/public/files/node-link.csv", function (error, data) {
+    d3.csv("/public/files/node-link-2.csv", function (error, data) {
         data.forEach(function (item) {
             mainChart.tmp_nodes.push(item.source);
             mainChart.tmp_nodes.push(item.target);
@@ -17,19 +21,30 @@ function Main() {
         mainChart.index_of_nodes = d3.map();
 
         for (var i = 0; i !== mainChart.tmp_nodes.length; ++i) {
-            var node = {id: mainChart.tmp_nodes[i]};
+            var degree = 0;
+            data.forEach(function (t) {
+                if (t.source === mainChart.tmp_nodes[i] || t.target === mainChart.tmp_nodes[i])
+                    degree += parseInt(t.count);
+            });
+            mainChart.rMax = mainChart.rMax > degree ? mainChart.rMax : degree;
+
+            var node = {id: mainChart.tmp_nodes[i], degree: degree};
             mainChart.nodes.push(node);
             mainChart.index_of_nodes.set(mainChart.tmp_nodes[i], i);
         }
 
         data.forEach(function (item) {
+            mainChart.bMax = mainChart.bMax > parseInt(item.count) ? mainChart.bMax : parseInt(item.count);
             var link = {
                 source: mainChart.index_of_nodes.get(item.source),
                 target: mainChart.index_of_nodes.get(item.target),
-                count: item.count
+                count: parseInt(item.count)
             };
             mainChart.links.push(link);
         });
+
+        mainChart.rScale.domain([1, mainChart.rMax]);
+        mainChart.bScale.domain([1, mainChart.bMax]);
 
         mainChart.svg = d3.select("#main")
             .append("svg")
@@ -51,7 +66,9 @@ function Main() {
             .attr("class", "link")
             .attr("stroke-opacity", 0.5)
             .attr("stroke", "gray")
-            .attr('stroke-width', 2);
+            .attr("stroke-width", function (d) {
+                return mainChart.bScale(d.count);
+            });
 
         mainChart.svg_nodes = mainChart.svg.selectAll(".node")
             .data(mainChart.nodes)
@@ -59,14 +76,13 @@ function Main() {
             .append("circle")
             .attr("class", "node")
             .attr("r", function (d) {
-                return 2;
+                return mainChart.rScale(d.degree);
             })
             .attr("opacity", 0.7)
             .attr("fill", "#0CC5E8")
             .call(mainChart.force.drag);
 
         mainChart.force.on("tick", function () {
-
             mainChart.svg_links.attr("x1", function (d) {
                 return d.source.x;
             });
@@ -88,7 +104,6 @@ function Main() {
             });
         });
     });
-
 }
 
-Main();
+MainChart();
